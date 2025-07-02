f08c2496f0f54fabf8e07a2ded688bfb42bbc9ef
alessandravitor <alessandrasilvav@gmail.com>
2025-07-02 14:01:59 -0300

    Projeto para curso de boas práticas em testes automatizados padrão triplo A

--- boas-praticas-java-testes/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
+++ boas-praticas-java-testes/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -17,48 +17,56 @@ public class CalculadoraProbabilidadeAdocaoTest {
 
     @Test
     public void deveRetornarProbabilidadeAltaParaPetComIdadeBaixaEPesoBaixoTest() {
+        // ARRANGE
         var cadastroPetDto = getCadastroPetDto(TipoPet.CACHORRO, 3, 8.5F);
         var cadastroAbrigoDto = getCadastroAbrigoDto();
         var abrigo = new Abrigo(cadastroAbrigoDto);
         var pet = new Pet(cadastroPetDto, abrigo);
-
+        // ACT
         var resultado = calculadoraProbabilidadeAdocao.calcular(pet);
+        // ASSERT
         assertEquals(resultado, ProbabilidadeAdocao.ALTA);
 
     }
 
     @Test
     public void deveRetornarProbabilidadeMediParaPetComIdadeAltaEPesoBaixoTest() {
+        // ARRANGE
         var cadastroPetDto = getCadastroPetDto(TipoPet.GATO, 11, 10F);
         var cadastroAbrigoDto = getCadastroAbrigoDto();
         var abrigo = new Abrigo(cadastroAbrigoDto);
         var pet = new Pet(cadastroPetDto, abrigo);
-
+        // ACT
         var resultado = calculadoraProbabilidadeAdocao.calcular(pet);
+        // ASSERT
         assertEquals(resultado, ProbabilidadeAdocao.MEDIA);
 
     }
 
     @Test
     public void deveRetornarProbabilidadeBaixaParaPetGatoComIdadeAltaEPesoAltoTest() {
+        // ARRANGE
         var cadastroPetDto = getCadastroPetDto(TipoPet.GATO, 10, 18.5F);
         var cadastroAbrigoDto = getCadastroAbrigoDto();
         var abrigo = new Abrigo(cadastroAbrigoDto);
         var pet = new Pet(cadastroPetDto, abrigo);
-
+        // ACT
         var resultado = calculadoraProbabilidadeAdocao.calcular(pet);
+        // ASSERT
         assertEquals(resultado, ProbabilidadeAdocao.BAIXA);
 
     }
 
     @Test
     public void deveRetornarProbabilidadeBaixaParaPetCachorroComIdadeAltaEPesoAltoTest() {
+        // ARRANGE
         var cadastroPetDto = getCadastroPetDto(TipoPet.CACHORRO, 15, 25.2F);
         var cadastroAbrigoDto = getCadastroAbrigoDto();
         var abrigo = new Abrigo(cadastroAbrigoDto);
         var pet = new Pet(cadastroPetDto, abrigo);
-
+        // ACT
         var resultado = calculadoraProbabilidadeAdocao.calcular(pet);
+        // ASSERT
         assertEquals(resultado, ProbabilidadeAdocao.BAIXA);
 
     }
